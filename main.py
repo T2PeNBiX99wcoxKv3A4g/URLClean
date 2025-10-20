b67ca9982ca3dfd130e9f2b5088717ce3593722b
yukiyosanako <179217493+T2PeNBiX99wcoxKv3A4g@users.noreply.github.com>
2025-10-20 22:28:26 +0800

    feat: Add workflow for pre-release and release automation. Update `pyproject.toml`, `.gitignore`, and `main.py`.

--- main.py
+++ main.py
@@ -15,22 +15,27 @@ def debug_output_control(debug: bool) -> None:
         ic.disable()
 
 
+def clean_url(url: str) -> str:
+    url_parse = ic(urlparse(url))
+    return ic(url_parse.scheme + "://" + url_parse.netloc + url_parse.path)
+
+
 def get_actual_url(url: str) -> str:
-    response = requests.head(url)
-    headers = ic(response.headers)
-    if "location" in headers:
-        return get_actual_url(headers["location"])
+    if url is None:
+        raise Exception("URL is None")
+    response = ic(requests.head(url))
+    if ic(response.status_code) == 302:
+        headers = ic(response.headers)
+        return get_actual_url(ic(headers["location"]))
     return url
 
 
 @app.command()
-def clean_url(url: str, debug: bool = False):
+def get(url: str, debug: bool = False):
     debug_output_control(debug)
     actual_url = get_actual_url(url)
-    url_parse = ic(urlparse(actual_url))
-    get_clean_url = ic(url_parse.scheme + "://" + url_parse.netloc + url_parse.path)
-    pyperclip.copy(get_clean_url)
-    typer.echo(f"Cleaned URL: {get_clean_url}")
+    pyperclip.copy(actual_url)
+    typer.echo(f"Cleaned URL: {actual_url}")
     typer.echo(f"Copied to clipboard")
 
 
